ab01394d5806944859c228a242dc29624c3719c8
Gabriel <mgabrielro@yahoo.com>
2017-03-21 08:11:44 +0000

    db infos

--- www/config/autoload/local.php
+++ www/config/autoload/local.php
@@ -1,3 +1,14 @@
 <?php
 
-return array();
+$user_email = 'mgabrielro@yahoo.com';
+
+return array(
+
+    'email' => $user_email,
+
+    'db' => array(
+        'username' => 'root',
+        'password' => '123456789',
+    ),
+
+);
